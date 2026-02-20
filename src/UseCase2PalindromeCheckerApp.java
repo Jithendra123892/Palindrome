ae4fec4c1b30eee280494d5e2dda2c4e6d8d66ba
MADDALA JITHENDRA <jithendramaddala009988@gmail.com>
2026-02-20 09:38:07 +0530

    Changed code

--- src/UseCase2PalindromeCheckerApp.java
+++ src/UseCase2PalindromeCheckerApp.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class UseCase2PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
-        System.out.print("Input Text: ");
-        String s=input.nextLine();
-        boolean t=true;
-        System.out.print("Input Text: "+s);
-        String r="";
-        for(int i=s.length()-1;i>=0;i--){
-               r+=s.charAt(i);
-        }
+
+        String s="madam";
+        System.out.println("Input Text: "+s);
+        boolean t= true;
+        String r="madam";
+
         if(s.equals(r)){
-            System.out.println("Is it a Palindrome:"+t);
+            System.out.println("Is it a Palindrome: "+t);
         }
         else{
             System.out.println("Not a Palindrome");
