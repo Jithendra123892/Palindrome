98f1719e2e167a16875a4cb459889143aa04cb17
MADDALA JITHENDRA <jithendramaddala009988@gmail.com>
2026-02-20 09:14:03 +0530

    Initial commit - Palindrome use cases

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input Text: ");
+        String s=input.nextLine();
+        boolean t=true;
+        System.out.print("Input Text: "+s);
+        String r="";
+        for(int i=s.length()-1;i>=0;i--){
+               r+=s.charAt(i);
+        }
+        if(s.equals(r)){
+            System.out.println("Is it a Palindrome:"+t);
+        }
+        else{
+            System.out.println("Note a Palindrome");
+        }
+
+    }
+}
