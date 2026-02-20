1cd93f39ddd1ff78a1449ceac6899449db31f3ef
MADDALA JITHENDRA <jithendramaddala009988@gmail.com>
2026-02-20 10:46:07 +0530

    UC3: Keep only UC3 file

--- src/UseCase1PalindromeCheckerApp.java
+++
@@ -1,7 +0,0 @@
- class UseCasePalindromeCheckerApp{
-    public static void main(String[] args) {
-      System.out.println("Welcome to the palindrome checker management system");
-      System.out.println("Version: 1.0");
-        System.out.println("System intialized successfully");
-    }
-}
