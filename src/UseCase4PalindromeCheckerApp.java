3c6f0b2be70af16868deca1ce3ac6f90b7b4de3b
MADDALA JITHENDRA <jithendramaddala009988@gmail.com>
2026-02-20 09:58:29 +0530

    UC4: Add new feature

--- src/UseCase4PalindromeCheckerApp.java
+++ src/UseCase4PalindromeCheckerApp.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class UseCase4PalindromeCheckerApp {
     public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Input Text: ");
+        String s=input.nextLine();
+       char r[]=s.toCharArray();
+       int i=0;
+       int j=s.length()-1;
+       boolean t=true;
+        while(i<j){
+            if(r[i]!=r[j]){
+                t=false;
+                break;
+            }
+            i++;
+            j--;
 
+        }
+        if(t){
+            System.out.println("the  string "+s+" is a palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome");
+        }
     }
 }
