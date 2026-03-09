123de1e9046e6ebfeb7d2bad7a3e3e46a977020c
MADDALA JITHENDRA <jithendramaddala009988@gmail.com>
2026-03-09 08:47:42 +0530

    UC8: Linked list based palindrome checker

--- src/UseCase8PalindromeCheckerApp.java
+++ src/UseCase8PalindromeCheckerApp.java
@@ -1,2 +1,93 @@
+import java.util.Scanner;
+
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
 public class UseCase8PalindromeCheckerApp {
+
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Find middle using fast & slow pointers
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow.next);
+
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a word to check palindrome:");
+        String input = sc.nextLine();
+
+        Node head = null;
+        Node tail = null;
+
+        // Convert string to linked list
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        boolean result = isPalindrome(head);
+
+        if (result) {
+            System.out.println("The given word is a Palindrome.");
+        } else {
+            System.out.println("The given word is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
 }
